6d614a466872fcff0e3d8006748c3fea221b2acf
Oleg Zolotar <zolotar1123@gmail.com>
2016-04-10 19:33:03 +0300

    implement showcase

--- app/src/main/java/com/example/oliver/customviews/MainActivity.java
+++ app/src/main/java/com/example/oliver/customviews/MainActivity.java
@@ -2,15 +2,52 @@ package com.example.oliver.customviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.github.amlcurran.showcaseview.OnShowcaseEventListener;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.SimpleShowcaseEventListener;
+import com.github.amlcurran.showcaseview.targets.ActionViewTarget;
+import com.github.amlcurran.showcaseview.targets.Target;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Target viewTarget = new ViewTarget(R.id.sb_text, this);
+        final ShowcaseView showcaseView = new ShowcaseView.Builder(this)
+                .setTarget(viewTarget)
+                .setContentTitle("Title ")
+                .setContentText("Message")
+//                .singleShot(42)
+                .setShowcaseEventListener(new SimpleShowcaseEventListener() {
+                    @Override
+                    public void onShowcaseViewHide(ShowcaseView showcaseView) {
+                        super.onShowcaseViewHide(showcaseView);
+                        Log.d(TAG, "onShowcaseViewHide");
+                    }
+                })
+                .setStyle(R.style.CustomShowcaseTheme3)
+                .build();
+
+        RelativeLayout.LayoutParams lps = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lps.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        lps.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        int margin = ((Number) (getResources().getDisplayMetrics().density * 12)).intValue();
+        lps.setMargins(margin, margin, margin, margin);
+        showcaseView.setButtonPosition(lps);
     }
 
     @Override
